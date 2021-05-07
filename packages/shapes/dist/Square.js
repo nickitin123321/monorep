4a5ca5ac4fc6f1fc6eb67794401e5ac9aef2dab0
N N <nickitin_2010@mail.ru>
2021-05-07 14:19:04 +0800

    feat: ssquare

--- packages/shapes/dist/Square.js
+++ packages/shapes/dist/Square.js
@@ -2,7 +2,7 @@ import Rect from './Rect';
 export default class Square extends Rect {
     constructor(side, centerPointX, centerPointY) {
         super(side, side, centerPointX, centerPointY);
-        this.name = 'square';
+        this.name = 'ssquare';
         this.side = side;
     }
 }
