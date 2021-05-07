c0461d7cc9645343100e21473f3e1455fb40ae1b
N N <nickitin_2010@mail.ru>
2021-05-07 13:19:53 +0800

    fix: name sqquare

--- packages/shapes/dist/Square.js
+++ packages/shapes/dist/Square.js
@@ -2,7 +2,7 @@ import Rect from './Rect';
 export default class Square extends Rect {
     constructor(side, centerPointX, centerPointY) {
         super(side, side, centerPointX, centerPointY);
-        this.name = 'square';
+        this.name = 'sqquare';
         this.side = side;
     }
 }
