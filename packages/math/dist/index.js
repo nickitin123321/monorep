afae5fa062743e1aa649cdf4629fd3f9ebabffc7
N N <nickitin_2010@mail.ru>
2021-05-14 17:26:59 +0800

    feat: new rect area

--- packages/math/dist/index.js
+++ packages/math/dist/index.js
@@ -12,7 +12,7 @@ function calculateArea(shape) {
     }
     if (shape instanceof Rect) {
         const rv = shape.longSide * shape.shortSide;
-        console.log('Rect area:', rv);
+        //console.log('Rect area:', rv);
         return rv;
     }
     if (shape instanceof Elipse) {
